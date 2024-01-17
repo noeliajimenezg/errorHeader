1d7341ef6985161a9d014846f952ea8f131d83f4
noeliajimenezg <37051537+noeliajimenezg@users.noreply.github.com>
2024-01-17 23:29:28 +0100

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,6 +2,10 @@ package com.example;
 
 import io.micronaut.runtime.Micronaut;
 
+/*
+grpcurl -import-path ~/Downloads/errorHeader-master/src/main/proto -proto ~/Downloads/errorHeader-master/src/main/proto/ErrorHeader.proto -vv -plaintext -d '{"name":"toto"}' localhost:50054 com.example.ErrorHeaderService/sendWithError
+grpcurl -import-path ~/Downloads/errorHeader-master/src/main/proto -proto ~/Downloads/errorHeader-master/src/main/proto/ErrorHeader.proto -vv -plaintext -d '{"name":"toto"}' localhost:50054 com.example.ErrorHeaderService/sendOk
+ */
 public class Application {
 
     public static void main(String[] args) {
